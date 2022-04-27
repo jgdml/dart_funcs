060d40afd2f960485a13de3610924690b4620dc5
jgdml <jgdml2801@gmail.com>
2022-04-27 20:55:30 -0300

    a3.1

--- bin/dart_funcs.dart
+++ bin/dart_funcs.dart
@@ -1,7 +1,9 @@
 import 'package:dart_funcs/a1_funcs/main_funcs.dart' as f;
 import 'package:dart_funcs/a2_async_funcs/main_async_funcs.dart' as async_f;
+import 'package:dart_funcs/a3/main_a3.dart' as a3;
 
 void main(List<String> arguments) {
   // f.init();
-  async_f.init();
+  // async_f.init();
+  a3.init();
 }
