39a42da0a98874392f1eafc5532e546f8553a7bd
jgdml <jgdml2801@gmail.com>
2022-05-04 21:40:20 -0300

    depuraçao

--- bin/dart_funcs.dart
+++ bin/dart_funcs.dart
@@ -1,11 +1,11 @@
 import 'package:dart_funcs/a1_funcs/main_funcs.dart' as f;
 import 'package:dart_funcs/a2_anon_funcs/main_anon_funcs.dart' as async_f;
 import 'package:dart_funcs/a3/main_a3.dart' as a3;
-import 'package:dart_funcs/a4_depuracao/a_depuracao.dart' as dep;
+import 'package:dart_funcs/a4_depuracao/b_exercicio.dart' as dep;
 
 void main(List<String> arguments) {
   // f.init();
   // async_f.init();
-  a3.init();
-  // dep.show();
+  // a3.init();
+  dep.show();
 }
---
+++ lib/a4_depuracao/ver_aprovacao/verificar_aprovacao.dart
@@ -0,0 +1,34 @@
+import 'package:dart_funcs/a4_depuracao/ver_aprovacao/calculo_nota.dart'
+    as nota;
+import 'package:dart_funcs/a4_depuracao/ver_aprovacao/calculo_presenca.dart'
+    as presenca;
+
+String verificarAprovacao(
+    double nota1,
+    double nota2,
+    double media,
+    int quantidadePresenca,
+    int cargaHorariaDisciplina,
+    double percentualMinimoPresenca) {
+  var aprovadoNota = verificarAprovacaoNota(nota1, nota2, media);
+  var aprovadoPresenca = verificarAprovacaoPresenca(
+      quantidadePresenca, cargaHorariaDisciplina, percentualMinimoPresenca);
+  var resultado = 'aprovado';
+  if (!aprovadoNota) {
+    resultado = 'reprovado por nota';
+  } else if (!aprovadoPresenca) {
+    resultado = 'reprovado por presença';
+  }
+  return resultado;
+}
+
+bool verificarAprovacaoNota(double nota1, double nota2, [double media = 6]) {
+  return (nota.calcularMedia(nota1, nota2) >= media);
+}
+
+bool verificarAprovacaoPresenca(int quantidadePresenca,
+    int cargaHorariaDisciplina, double percentualMinimoPresenca) {
+  return (presenca.calcularPercentualPresenca(
+          quantidadePresenca, cargaHorariaDisciplina) >
+      percentualMinimoPresenca);
+}
