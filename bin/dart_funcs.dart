23929ad0426037804c916eb5957e5d5c42e5aa0d
jgdml <jgdml2801@gmail.com>
2022-05-04 22:30:35 -0300

    inicio aula 5

--- bin/dart_funcs.dart
+++ bin/dart_funcs.dart
@@ -3,9 +3,12 @@ import 'package:dart_funcs/a2_anon_funcs/main_anon_funcs.dart' as async_f;
 import 'package:dart_funcs/a3/main_a3.dart' as a3;
 import 'package:dart_funcs/a4_depuracao/b_exercicio.dart' as dep;
 
+import 'package:dart_funcs/a5_except/main_except.dart' as except;
+
 void main(List<String> arguments) {
   // f.init();
   // async_f.init();
   // a3.init();
-  dep.show();
+  // dep.show();
+  except.init();
 }
