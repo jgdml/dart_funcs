4b02f74edbe637fb323ea2291ad280585889110b
jgdml <jgdml2801@gmail.com>
2022-05-21 19:10:59 -0300

    funções aula 05 + testes

--- lib/a5_teste_unidade/atv_teste.dart
+++ lib/a5_teste_unidade/atv_teste.dart
@@ -1,53 +1,109 @@
-import 'dart:convert';
-
 void show() {
-  var pagamentoList = [
-    Pagamento("Cartão", 10),
-    Pagamento("Boleto", 15),
-    Pagamento("Depósito", 20),
-  ];
-  var cmp = Compra(200);
-  cmp.pagamento = pagamentoList[0];
+  var cmp = Compra(200, "Cartão");
+
+  cmp.cupons.add(Cupom(
+    desconto: 5,
+    usos: 100,
+    tipo: CupomDescontoTipo.fixo,
+    validade: DateTime(2023, 01, 01),
+  ));
+  cmp.cupons.add(Cupom(
+    desconto: 10,
+    usos: 100,
+    tipo: CupomDescontoTipo.porcentagem,
+    validade: DateTime(2023, 01, 01),
+  ));
+  cmp.cupons.add(Cupom(
+    desconto: 25,
+    usos: 100,
+    tipo: CupomDescontoTipo.fixo,
+    validade: DateTime(2023, 12, 25),
+  ));
+
+  cmp.finalizarCompra();
 }
 
-class Pagamento {
-  String tipo;
-  int porcentagemDesconto;
-
-  Pagamento(this.tipo, this.porcentagemDesconto);
+enum CupomDescontoTipo {
+  fixo,
+  porcentagem,
 }
 
 class Cupom {
-  String tipo;
+  CupomDescontoTipo tipo;
   double desconto;
   DateTime validade;
+  int? usos;
 
-  Cupom(this.tipo, this.desconto, this.validade);
-
-  static void gerarCupom(String tipo, double desconto, DateTime validade) {
-    var codigo = utf8.encode("$tipo,$desconto,$validade");
-    print(codigo);
-    print(utf8.decode(codigo));
-  }
+  Cupom({required this.tipo, required this.desconto, required this.validade, this.usos});
 
   static bool validarCupom(Cupom cupom) {
-    if (cupom.validade.difference(DateTime.now()).isNegative) {
-      return false;
+    bool validadeValida = !cupom.validade.difference(DateTime.now()).isNegative;
+    bool restaUsos = (cupom.usos ?? 1) > 0 ;
+    if (validadeValida && restaUsos) {
+      return true;
     }
-    return true;
+    return false;
   }
 }
 
 class Compra {
-  Pagamento? pagamento;
-  List<Cupom>? cupons;
+  List<Cupom> cupons = [];
+  String pagamento;
   double precoTotal = 0;
 
-  Compra(this.precoTotal);
+  Compra(this.precoTotal, this.pagamento);
 
   void _calcularDescontoPagamento() {
-    precoTotal * (pagamento!.porcentagemDesconto / 100);
+    var pagamentoList = <String, int>{
+      "Cartão": 5,
+      "Boleto": 15,
+      "Depósito": 20,
+    };
+    print("\nTipo do Pagamento: \n$pagamento: ${pagamentoList[pagamento]}%");
+    precoTotal *= 1 - (pagamentoList[pagamento]! / 100);
   }
 
-  void _calcularCupons() {}
+  void _calcularCupons() {
+    double novoPreco = precoTotal;
+
+    print("\nCupons: ");
+
+    for (Cupom c in cupons) {
+      if (Cupom.validarCupom(c)) {
+        if (c.tipo == CupomDescontoTipo.fixo) {
+          novoPreco -= c.desconto;
+          print("R\$${c.desconto.toStringAsFixed(2)}");
+        } else if (c.tipo == CupomDescontoTipo.porcentagem) {
+          novoPreco *= 1 - (c.desconto / 100);
+          print("-${c.desconto.toInt()}%");
+        }
+      } else {
+        print("Cupom Inválido");
+      }
+    }
+
+    if (novoPreco < precoTotal / 2) {
+      print("Os cupons não podem reduzir mais do que metade do preço, remova alguns cupons e tente novamente.");
+    } else {
+      precoTotal = novoPreco;
+    }
+  }
+
+  static int calcularBonusDeCupons(int qntdCupons) {
+    int descontoBonus = 0;
+    descontoBonus = 2 * (qntdCupons ~/ 3);
+    
+    return descontoBonus;
+  }
+
+  void finalizarCompra() {
+    _calcularDescontoPagamento();
+    _calcularCupons();
+    
+    int porcentagemDescontoBonus = calcularBonusDeCupons(cupons.length);
+    precoTotal -= (precoTotal * (porcentagemDescontoBonus / 100));
+    print("\nDesconto bonus: $porcentagemDescontoBonus%");
+
+    print("\nTotal: R\$${precoTotal.toStringAsFixed(2)}");
+  }
 }
--- test/dart_funcs_test.dart
+++ test/dart_funcs_test.dart
@@ -1,7 +1,99 @@
 import 'package:dart_funcs/a5_teste_unidade/aprovacao_nota.dart';
+import 'package:dart_funcs/a5_teste_unidade/atv_teste.dart';
 import 'package:test/test.dart';
 
 void main() {
+  group("Teste função validar cupom", () {
+    test("Cupom com data passada", () {
+      expect(
+          Cupom.validarCupom(Cupom(
+            desconto: 5,
+            tipo: CupomDescontoTipo.fixo,
+            validade: DateTime.now().subtract(Duration(days: 1)),
+          )),
+          false);
+    });
+
+    test("Cupom com data futura", () {
+      expect(
+          Cupom.validarCupom(Cupom(
+            desconto: 5,
+            tipo: CupomDescontoTipo.fixo,
+            validade: DateTime.now().add(Duration(days: 1)),
+          )),
+          true);
+    });
+
+    test("Cupom sem usos restantes", () {
+      expect(
+          Cupom.validarCupom(Cupom(
+            usos: 0,
+            desconto: 5,
+            tipo: CupomDescontoTipo.fixo,
+            validade: DateTime.now().add(Duration(days: 1)),
+          )),
+          false);
+    });
+
+    test("Cupom com usos restantes", () {
+      expect(
+          Cupom.validarCupom(Cupom(
+            usos: 1,
+            desconto: 5,
+            tipo: CupomDescontoTipo.fixo,
+            validade: DateTime.now().add(Duration(days: 1)),
+          )),
+          true);
+    });
+
+    test("Cupom com usos ilimitados", () {
+      expect(
+          Cupom.validarCupom(Cupom(
+            desconto: 5,
+            tipo: CupomDescontoTipo.fixo,
+            validade: DateTime.now().add(Duration(days: 1)),
+          )),
+          true);
+    });
+    test("Cupom com uso negativo", () {
+      expect(
+          Cupom.validarCupom(Cupom(
+            usos: -1,
+            desconto: 5,
+            tipo: CupomDescontoTipo.fixo,
+            validade: DateTime.now().add(Duration(days: 1)),
+          )),
+          false);
+    });
+  });
+
+  group("Teste função calcular desconto bonus", () {
+
+    test("Quantidade de cupons suficiente", () {
+      expect(Compra.calcularBonusDeCupons(3), 2);
+    });
+
+    test("Quantidade de cupons insuficiente", () {
+      expect(Compra.calcularBonusDeCupons(2), 0);
+    });
+
+    test("Quantidade de cupons perto do proximo valor", () {
+      expect(Compra.calcularBonusDeCupons(8), 4);
+    });
+
+    test("Sem cupons utilizados", () {
+      expect(Compra.calcularBonusDeCupons(0), 0);
+    });
+
+    test("Quantidade de cupons negativa", () {
+      expect(Compra.calcularBonusDeCupons(-1), 0);
+    });
+  });
+
+
+  //////////////
+
+
   group("Teste de cálculo da média", () {
     test("Média maior que a média definida", () {
       expect(calcularMedia(nota1: 6, nota2: 8), 7);
